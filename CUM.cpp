1cc119ac2cc6317c6cac2063dc401e4c19da757d
mkdeosm <>
2022-02-28 21:14:31 +0200

    CUM
    v1.5

---
+++ CUM.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "CUM v.1.5" << endl;
+    cout << "Realese by Vladislav Yastribov" << endl;
+    cout << "" << endl;
+    cout << "Nillerusr gay" << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl;
+    cout << "CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM CUM " << endl; 
+} 
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mk_cum
-cum
+this is a super big cum made from the heart for everyone who wants to lick a little in the cum.
+CUM is -
+Camera Updater Macros
+
+# thanks for download!
+good project very nice!
