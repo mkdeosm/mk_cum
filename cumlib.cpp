33da8ae83356fb778ca86929b684b7629f2ff19d
mkdeosm <100498776+mkdeosm@users.noreply.github.com>
2022-03-01 22:32:05 +0200

    v1.7
    cum test

---
+++ cumlib.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int info_player_number;
+    cout << "\nYour Number: " << endl;
+    cin >> info_player_number;
+    cout << "Welcome to CUM WORLD! " << info_player_number << endl;
+    int a;
+    cout << "You like cum? [Y/n]" << endl;
+    cin >> a;
+    cout << "very god!!!! have fun!" << endl;
+    cout << "\nexit" << endl;
+    
+}
